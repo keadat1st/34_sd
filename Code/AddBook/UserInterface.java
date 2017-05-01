47ddc45018a95e69bc793c9fececfa81f1a0ec28
clbokea <clbo@kea.dk>
2017-05-01 11:31:09 +0200

    pdf added

---
+++ Code/AddBook/UserInterface.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class UserInterface
+{
+    public static void main(String[] args)
+    {
+        boolean more = true;
+
+        while(more)
+        {
+             Scanner scan = new Scanner(System.in);
+
+            // Vælg mellem add book og andre
+            System.out.print("Title: ");
+            String title = scan.nextLine();
+
+            System.out.print("Author: ");
+            String author = scan.nextLine();
+
+            System.out.print("ID: ");
+            int id = scan.nextInt();
+
+            Library lib = new Library();
+
+             System.out.println(lib.addBook(title,author, id));
+
+            System.out.print("Add more books? ");
+            String morestring = scan.next();
+            if(morestring.equals("yes"))
+            {
+                more = true;
+            } else {
+                more = false;
+            }
+            
+        }
+       
+
+    }
+}
